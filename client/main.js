7914acb63235d59f75a7bcf6c6c6c355b89b154d
Berkan Eskikaya <berkan.eskikaya@gmail.com>
2016-05-08 13:12:37 +0100

    initial code, after 'meteor add twbs:bootstrap fortawesome:fontawesome iron:router'

---
+++ client/main.js
@@ -0,0 +1,33 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+
+Router.configure({
+  layoutTemplate: "ApplicationLayout"
+});
+
+Router.route('/', function() {
+  this.render('navbar', {to:'header'});
+  this.render('introduction', {to:'main'});
+});
+
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
